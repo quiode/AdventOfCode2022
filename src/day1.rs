1e675c78d9defbf83e323244ca16c151e1ab73ec
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2022-12-04 13:41:07 +0100

    problem 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,13 +1,13 @@
 use std::{ fs::File, io::{ BufRead, BufReader, Lines } };
 
 pub fn main() {
-    let input_file = File::open("input.txt").unwrap();
-    let lines = BufReader::new(input_file).lines();
+    let lines = get_lines("input.txt");
 
-    println!("Biggest Cal: {}", calc_from_lines(lines));
+    // println!("Problem 1: {}", problem1(lines));
+    println!("Problem 2: {}", problem2(lines));
 }
 
-fn calc_from_lines(lines: Lines<BufReader<File>>) -> i32 {
+fn problem1(lines: Lines<BufReader<File>>) -> i32 {
     let mut biggest_cal = 0;
     let mut current_cal = 0;
 
@@ -28,14 +28,62 @@ fn calc_from_lines(lines: Lines<BufReader<File>>) -> i32 {
     biggest_cal
 }
 
+fn problem2(lines: Lines<BufReader<File>>) -> i32 {
+    let mut top3: [i32; 3] = [0; 3];
+
+    let mut current_cal = 0;
+
+    for line in lines {
+        let line = line.unwrap();
+        if line.trim().len() == 0 {
+            top3.sort();
+
+            if top3[0] < current_cal {
+                top3[0] = current_cal;
+            }
+            current_cal = 0;
+        } else {
+            current_cal += line.parse::<i32>().unwrap();
+        }
+    }
+
+    top3.sort();
+
+    if top3[0] < current_cal {
+        top3[0] = current_cal;
+    }
+
+    let mut sum = 0;
+    for i in top3 {
+        sum += i;
+    }
+    sum
+}
+
+fn get_lines(path: &str) -> Lines<BufReader<File>> {
+    let input_file = File::open(path).unwrap();
+    BufReader::new(input_file).lines()
+}
+
 #[cfg(test)]
 mod test {
-    use std::{ fs::File, io::{ Write, self, BufRead } };
+    use std::{ fs::File, io::{ Write, self, BufRead, Lines, BufReader }, thread::Result };
 
-    use super::calc_from_lines;
+    use super::{ problem1, get_lines, problem2 };
 
     #[test]
-    fn main() {
+    fn problem1_test() {
+        let result = problem1(get_sample_lines());
+        assert_eq!(result, 24000);
+    }
+
+    #[test]
+    fn problem2_test() {
+        let result = problem2(get_sample_lines());
+        assert_eq!(result, 45000);
+    }
+
+    fn get_sample_lines() -> Lines<BufReader<File>> {
         let input = "1000
 2000
 3000
@@ -56,10 +104,6 @@ mod test {
         file.write_all(&input.as_bytes()).unwrap();
         drop(file);
 
-        let file = File::open("test-input.txt").unwrap();
-
-        let result = calc_from_lines(io::BufReader::new(file).lines());
-
-        assert_eq!(result, 24000);
+        get_lines("test-input.txt")
     }
 }

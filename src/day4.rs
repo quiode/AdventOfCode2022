7cc322f401fba29fea0c2f4d41d53329de7103bd
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2022-12-04 19:18:57 +0100

    day 4 problem 2

--- src/day4.rs
+++ src/day4.rs
@@ -5,8 +5,8 @@ use crate::line_manager;
 pub fn main() {
     let lines = line_manager::get_lines(line_manager::FILE);
 
-    println!("Problem 1: {}", problem1(lines));
-    // println!("Problem 2: {}", problem2(lines));
+    // println!("Problem 1: {}", problem1(lines));
+    println!("Problem 2: {}", problem2(lines));
 }
 
 fn problem1(lines: Lines<BufReader<File>>) -> i32 {
@@ -19,6 +19,23 @@ fn problem1(lines: Lines<BufReader<File>>) -> i32 {
         let range2 = parse_range(ranges[1]);
 
         if contains(&range1, &range2) {
+            sum += 1;
+        }
+    }
+
+    sum
+}
+
+fn problem2(lines: Lines<BufReader<File>>) -> i32 {
+    let mut sum = 0;
+
+    for line in lines {
+        let line = line.unwrap();
+        let ranges = line.split(",").collect::<Vec<_>>();
+        let range1 = parse_range(ranges[0]);
+        let range2 = parse_range(ranges[1]);
+
+        if overlapps(&range1, &range2) {
             sum += 1;
         }
     }
@@ -41,9 +58,14 @@ fn contains(range1: &Range<i32>, range2: &Range<i32>) -> bool {
         (range1.start >= range2.start && range1.end <= range2.end)
 }
 
+/// returns true if one range overlapps with the other
+fn overlapps(range1: &Range<i32>, range2: &Range<i32>) -> bool {
+    range1.start <= range2.end && range2.start <= range1.end
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::line_manager;
+    use crate::{ line_manager, day4::problem2 };
 
     use super::problem1;
 
@@ -63,5 +85,23 @@ mod tests {
         );
 
         assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn problem2_test() {
+        let result = problem2(
+            line_manager::create_lines(
+                "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+",
+                line_manager::TEST_FILE
+            )
+        );
+
+        assert_eq!(result, 4);
     }
 }
